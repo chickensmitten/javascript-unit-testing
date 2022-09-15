e74951f3f90e368c7ce5a0f54edbc61884a6ee29
sheng yeong <chia.shengyeong@gmail.com>
2022-09-15 08:58:05 +0800

    more tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,6 +2,44 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should summarise all number values in an array", () => {
-  const result = add([1,2,3]);
-  expect(result).toBe(6);
+  // Arrange
+  const numbers = [1, 2];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  );  
+  expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if at least invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numberic string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );    
+  expect(result).toBe(expectedResult);
+})
+
+it("should yield 0 if an empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+})
