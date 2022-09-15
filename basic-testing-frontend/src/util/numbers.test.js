ee195548ddfb112a73e857bc64cc08c2d925f0ec
sheng yeong <chia.shengyeong@gmail.com>
2022-09-15 10:32:08 +0800

    integration tests

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,30 +1,55 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from "./numbers";
+import { describe, it, expect } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it("should transform a string number to a number of type number", () =>{
-  const input ="1";
-
-  const result = transformToNumber(input);
-
-  expect(result).toBeTypeOf("number");
+describe("transformToNumber()", () => {
+  it("should transform a string number to a number of type number", () =>{
+    const input ="1";
+  
+    const result = transformToNumber(input);
+  
+    expect(result).toBeTypeOf("number");
+  });
+  
+  it("should transform a string number to a number of type number", () =>{
+    const input ="1";
+  
+    const result = transformToNumber(input);
+  
+    expect(result).toBe(+input);
+  });
+  
+  it("should yield NaN for non-transformable values", () => {
+    const input = "invalid";
+    const input2 = {};
+  
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2); 
+  
+    expect(result).toBeNaN();
+    // you can do multiple tests, but typically, you should only have one
+    expect(result2).toBeNaN();
+  });
+  
 });
 
-it("should transform a string number to a number of type number", () =>{
-  const input ="1";
+describe("cleanNumbers()", () => {
+  // this is an integration test because despite all the functions in side this function works.
+  // they might not work correctly together. So we will need this test.
+  it("should return an array of number values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2"];
 
-  const result = transformToNumber(input);
+    const cleanedNumbers = cleanNumbers(numberValues);
 
-  expect(result).toBe(+input);
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
+
+  it("should throw an error if an array with at least one empty string is provided", () => {
+    const numberValues = ["", 1];
+
+    const cleanFn = () => cleanNumbers(numberValues);
+
+    expect(cleanFn).toThrow();
+  })
+
 });
 
-it("should yield NaN for non-transformable values", () => {
-  const input = "invalid";
-  const input2 = {};
-
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2); 
-
-  expect(result).toBeNaN();
-  // you can do multiple tests, but typically, you should only have one
-  expect(result2).toBeNaN();
-});
