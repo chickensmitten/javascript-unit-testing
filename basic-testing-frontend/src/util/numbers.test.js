ab86df791416ffe64ab09431cbee77ed225af373
sheng yeong <chia.shengyeong@gmail.com>
2022-09-15 10:44:27 +0800

    toBe vs toEqual

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -43,6 +43,16 @@ describe("cleanNumbers()", () => {
     expect(cleanedNumbers[0]).toBeTypeOf("number");
   });
 
+  it("should return an array of number values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2"];
+
+    const cleanedNumbers = cleanNumbers(numberValues);
+
+    // toBe doesnt work for array because objects like [] or {} are reference values, they are not equal.
+    // use toEqual instead
+    expect(cleanedNumbers).toEqual([1, 2]);
+  });  
+
   it("should throw an error if an array with at least one empty string is provided", () => {
     const numberValues = ["", 1];
 
